3365791da241b19dda96dc5ed8edd9bddc51ed4f
YaroslavBandura <yaroslav.bandura@simovative.com>
2025-11-05 09:40:58 +0100

    composer

--- public/index.php
+++ public/index.php
@@ -1,6 +1,33 @@
 <?php
+
 use SimoBanduraYaroslav\Blogmaster\Calculator\Calculator;
 
 require_once '../vendor/autoload.php';
-$calculator= new Calculator();
-echo $calculator->calculate();
+$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals();
+$test = $queryParam = $request->getParsedBody();
+
+$result = "";
+if ($request->getServerParams()["REQUEST_METHOD"] === "POST") {
+    $a = (float)$test["a"];
+    $b = (float)$test["b"];
+    $operation = $test["operation"];
+
+    $calc = new Calculator();
+
+    $result = $calc->calculate($a, $operation, $b);
+}
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../src/Calculator/Templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__ . '/../data/cache',
+    'autoescape' => 'html',
+    'auto_reload' => true,
+]);
+
+echo $twig->render('calculator.twig', [
+    'result' => $result,
+    'a' => $a,
+    'b' => $b,
+    'operation' => $operation,
+]);
+?>
--- src/Calculator/Calculator.php
+++ src/Calculator/Calculator.php
@@ -1,9 +1,51 @@
 <?php
- namespace SimoBanduraYaroslav\Blogmaster\Calculator;
 
-class Calculator{
+namespace SimoBanduraYaroslav\Blogmaster\Calculator;
 
-    public function calculate(){
-        return 1;
+class Calculator
+{
+    private function add(float $a, float $b): float
+    {
+        return $a + $b;
+    }
+
+    private function subtract(float $a, float $b): float
+    {
+        return $a - $b;
+    }
+
+    private function multiply(float $a, float $b): float
+    {
+        return $a * $b;
+    }
+
+    private function divide(float $a, float $b): float|string
+    {
+        if ($b == 0) {
+            return "Error: Division by zero";
+        }
+        return $a / $b;
+    }
+
+    public function calculate(float $a, string $operation, float $b): float|string
+    {
+        switch ($operation) {
+            case 'add':
+                $result = $this->add($a, $b);
+                break;
+            case 'subtract':
+                $result = $this->subtract($a, $b);
+                break;
+            case 'multiply':
+                $result = $this->multiply($a, $b);
+                break;
+            case 'divide':
+                $result = $this->divide($a, $b);
+                break;
+            default:
+                $result = "Error";
+        }
+        return $result;
     }
 }
+
